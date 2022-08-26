cc36b3cd3adba506ed554188471fc3a391e263c0
anupam <anupam.b@inapp.com>
2022-08-26 09:16:34 +0530

    Changed route to batches

--- website/views.py
+++ website/views.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
-def home():
+@views.route('/batches')
+def batches():
     return render_template('batches.html')
