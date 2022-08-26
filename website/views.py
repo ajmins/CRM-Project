cf42c4e8a735ddc584311d55ec9e3effb8f269ee
KripaVS <kripa.ev@inapp.com>
2022-08-26 17:20:19 +0530

    qualification+ categories

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from .models import Category
+from .models import Category, Qualifications
 
 views = Blueprint('views', __name__)
 
@@ -9,8 +9,10 @@ def batches():
 
 @views.route('/categories')
 def categories():
-    return render_template('categories.html')
+    categories=Category.query.all()
+    return render_template('categories.html',categories=categories)
  
 @views.route('/qualification')
 def qualifications():
-    return render_template('qualification.html')
+    qualifications=Qualifications.query.all()
+    return render_template('qualification.html',qualifications=qualifications)
