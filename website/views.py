1deb8d39a539c5cde607c09012643768ab2e4031
prince-inapp <prince.j@inapp.com>
2022-08-30 23:58:14 +0530

    some changes

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from .models import Category
+from .models import Category, Courses
 
 views = Blueprint('views', __name__)
 
@@ -14,3 +14,22 @@ def categories():
 @views.route('/qualification')
 def qualifications():
     return render_template('qualification.html')
+
+@views.route('/courses', methods=['GET', 'POST'])
+def courses():
+    if request.method == 'POST':
+        courseName = request.form.get('courseName')
+        courseCategory = request.form.get('courseCategory')
+        courseQualification = request.form.get('courseQualification')
+        courseInstructor = request.form.get('courseInstructor')
+        courseStatus = request.form.get('courseStatus')
+        courseComments = request.form.get('courseComments')
+
+        
+
+        # course = Courses(courseName=courseName, courseCategory=courseCategory, courseQualification=courseQualification, courseInstructor=courseInstructor, courseStatus=courseStatus, courseComments=courseComments)
+        # db.session.add(course)
+        # db.session.commit()
+        return render_template('courses.html', categories= Category.query.all())
+    elif request.method == 'GET':
+        return render_template('courses.html', courses = Courses.query.all())
