21fccfe609a0df7a8d3c057d761ef7300a2954fb
anupamb-2000 <anupam.b@inapp.com>
2022-08-31 00:29:23 +0530

    Added users and dashboard

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -15,48 +15,93 @@ function clearFilters() {
 }
 
 // Materialize Select
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
-  });
+});
+
+// Search User
+function searchUser() {
+    searchBy = document.getElementById('searchBy').value
+    searchConstraint = document.getElementById('searchConstraint').value
+    
+    fetch('/users/' + searchBy + '/' + searchConstraint, {
+        method: 'GET'
+    })
+        .then(() => window.location.href = "/users/" + searchBy + '/' + searchConstraint);
+}
+
+// Filter Batch
+function applyRoleFilters() {
+    inputs = document.querySelectorAll('.filterCheckbox:checked')
+    let roles = { 'roles': [] }
+    inputs.forEach(ip => {
+        roles['roles'].push(ip.value)
+    });
+    if(roles['roles'] == []){
+        window.location.href = "/users"
+    }
+    fetch('/users', {
+        method: 'GET'
+    })
+        .then(() => window.location.href = "/users?roles=" + roles['roles'])
+}
+
+// Back to users
+function userBack() {
+    window.location.href = "/users"
+}
 
 // Delete Category
-function deleteCategory(categoryId){
+function deleteCategory(categoryId) {
     fetch('/categories/' + categoryId, {
         method: 'DELETE'
     })
-    .then(() => window.location.href="/categories");
+        .then(() => window.location.href = "/categories");
 }
 
 // Search Category
-function searchCategory(){
+function searchCategory() {
     searchBy = document.getElementById('searchBy').value
     searchConstraint = document.getElementById('searchConstraint').value
     fetch('/categories/' + searchBy + '/' + searchConstraint, {
         method: 'GET'
     })
-    .then(() => window.location.href="/categories/" + searchBy + '/' + searchConstraint);
+        .then(() => window.location.href = "/categories/" + searchBy + '/' + searchConstraint);
 }
 
 // Back to categories
-function categoryBack(){
-    window.location.href="/categories"
+function categoryBack() {
+    window.location.href = "/categories"
+}
+
+// Filter Batch
+function applyFilters() {
+    inputs = document.querySelectorAll('.filterCheckbox:checked')
+    let categories = { 'categories': [] }
+    inputs.forEach(ip => {
+        categories['categories'].push(ip.value)
+    });
+    fetch('/batches', {
+        method: 'GET'
+    })
+        .then(() => window.location.href = "/batches?categories=" + categories['categories'])
 }
 
 // Close/Open Batch
-function toggleBatch(){
+function toggleBatch() {
     toggleSwitch = document.getElementById('batchSwitch')
     if (toggleSwitch.checked) {
         alert(Boolean(toggleSwitch.value))
-    } 
+    }
     else {
         alert(Boolean(''))
     }
 }
 
-function editBatch(batchId){
+function editBatch(batchId) {
     strength = document.getElementById('editBatchStrength' + batchId).value
-    if(strength == 0){
+    if (strength == 0) {
         strength = document.getElementById('editBatchStrength' + batchId).placeholder
     }
     fetch('/batches/' + batchId, {
@@ -71,40 +116,113 @@ function editBatch(batchId){
             batchEndDate: document.getElementById('editBatchEndDate' + batchId).placeholder
         })
     })
-    .then(() => window.location.href="/batches");
+        .then(() => window.location.href = "/batches");
 }
 
 // Delete Batch
-function deleteBatch(batchId){
+function deleteBatch(batchId) {
     fetch('/batches/' + batchId, {
         method: 'DELETE'
     })
-    .then(() => window.location.href="/batches");
+        .then(() => window.location.href = "/batches");
 }
 
 // Search Batch
-function searchBatch(date){
-    if (date != 'date'){
+function searchBatch(date) {
+    if (date != 'date') {
         searchBy = document.getElementById('searchBy').value
         searchConstraint = document.getElementById('searchConstraint').value
     }
-    else{
+    else {
         searchBy = 'date'
         searchConstraint = document.getElementById('branchStartDateSearch').value
     }
     fetch('/batches/' + searchBy + '/' + searchConstraint, {
         method: 'GET'
     })
-    .then(() => window.location.href="/batches/" + searchBy + '/' + searchConstraint);
+        .then(() => window.location.href = "/batches/" + searchBy + '/' + searchConstraint);
 }
 
 // Back to batches
-function batchBack(){
-    window.location.href="/batches"
+function batchBack() {
+    window.location.href = "/batches"
 }
 
 // Get date picked
-function getDate(){
+function getDate() {
     console.log(document.getElementById('batchStartDate').value)
 }
-    
+
+// Building Chart
+function BuildChart(labels, values, chartTitle) {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels, // Our labels
+            datasets: [{
+                fill: false,
+                label: chartTitle, // Name the series
+                data: values, // Our values
+                borderColor: [ // Add custom color borders
+                    'rgba(82, 171, 152, 1)'
+                ],
+                borderWidth: 2 // Specify bar border width
+            }]
+        },
+        options: {
+            responsive: true, // Instruct chart js to respond nicely.
+            maintainAspectRatio: false, // Add to prevent default behavior of full-width/height 
+            scales: {
+                xAxes: [{
+                   gridLines: {
+                      display: false
+                   }
+                }],
+                yAxes: [{
+                    gridLines: {
+                        display: false
+                        },
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+           }
+        }
+    });
+    return myChart;
+}
+
+var table = document.getElementById('dataTable');
+var json = []; // First row needs to be headers 
+var headers = [];
+for (var i = 0; i < table.rows[0].cells.length; i++) {
+    headers[i] = table.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi, '');
+}
+
+// Go through cells 
+for (var i = 1; i < table.rows.length; i++) {
+    var tableRow = table.rows[i];
+    var rowData = {};
+    for (var j = 0; j < tableRow.cells.length; j++) {
+        rowData[headers[j]] = tableRow.cells[j].innerHTML;
+    }
+
+    json.push(rowData);
+}
+
+console.log(json);
+
+// Map JSON values back to label array
+var labels = json.map(function (e) {
+    return e.date;
+});
+console.log(labels);
+
+// Map JSON values back to values array
+var values = json.map(function (e) {
+    return e.logincount;
+});
+console.log(values);
+
+var chart = BuildChart(labels, values, "Login Count of users");
--- website/views.py
+++ website/views.py
@@ -1,11 +1,43 @@
 from flask import Blueprint, render_template, request, jsonify, url_for, redirect
 from . import db
-from .models import Category, Batches, Courses
+from .models import Category, Batches, Courses, Users, ActivityLog
 import json
+from sqlalchemy import func, Date
+from datetime import date
 
 views = Blueprint('views', __name__)
 
 categories =[]
+
+@views.route('/dashboard')
+def dashboard():
+    users = Users.query.all()
+    dates = ActivityLog.query.with_entities(func.cast(ActivityLog.time, Date).label('Date'), func.count(ActivityLog.userId).label('logincount')).group_by(func.cast(ActivityLog.time, Date)).all()
+    # dates = ActivityLog.query(cast(ActivityLog.time, Date)).distinct().all()
+    return render_template('dashboard.html', users=users, dates=dates)
+
+@views.route('/users')
+def  users():
+    users = Users.query.all()
+    if request.args :
+        print(request.args.get('roles').split(','))
+        listAll = False
+        users = Users.query.filter(Users.userRoleId.in_(request.args.get('roles').split(','))).all()
+        if len(request.args.get('roles').split(',')) == 2:
+            listAll = True
+        elif request.args.get('roles').split(',') == ['']:
+            listAll = True
+            users = Users.query.all()
+        return render_template('users.html', users=users, listAll=listAll)
+    return render_template('users.html', users=users, listAll=True)
+
+@views.route('/users/<searchBy>/<searchConstraint>')
+def searchUser(searchBy, searchConstraint):
+    if searchBy == 'id':
+        users = Users.query.filter(Users.userId.like("%"+searchConstraint+"%")).all()
+    elif searchBy == 'name':
+        users = Users.query.filter(Users.userName.like("%"+searchConstraint+"%")).all()
+    return render_template('users.html', users=users, listAll=False)
 
 @views.route('/batches', methods=['GET', 'POST'])
 def batches():
@@ -23,7 +55,18 @@ def batches():
         db.session.commit()
     batches = Batches.query.all()
     courses = Courses.query.with_entities(Courses.courseId, Courses.courseName).distinct().all()
-    return render_template('batches.html', batches=batches[::-1], listAll=True, courses=courses)
+    categories = Category.query.with_entities(Category.categoryId, Category.categoryName).distinct().all()
+    if request.args :
+        print(request.args.get('categories').split(','))
+        listAll = False
+        batches = Batches.query.filter(Batches.batchStatus.in_((request.args.get('categories')).split(','))).all()
+        if len(request.args.get('categories').split(',')) == 2:
+            listAll = True
+        elif request.args.get('categories').split(',') == ['']:
+            listAll = True
+            batches = Batches.query.all()
+        return render_template('batches.html', batches=batches[::-1], listAll=listAll, courses=courses, categories=categories)
+    return render_template('batches.html', batches=batches[::-1], listAll=True, courses=courses, categories=categories)
 
 @views.route('/batches/<batchId>', methods=['DELETE'])
 def deleteBatch(batchId):
@@ -62,7 +105,9 @@ def searchBatch(searchBy, searchConstraint):
         searchConstraint = dateList[2] + "-" + f"{(months.index(dateList[0])+1):02}" + "-" + f"{dateList[1][:-1]:02}"
         print(searchConstraint)
         batches = Batches.query.filter(Batches.batchStartDate.like("%"+searchConstraint+"%")).all()
-    return render_template('batches.html', batches=batches, listAll=False)
+    courses = Courses.query.with_entities(Courses.courseId, Courses.courseName).distinct().all()
+    categories = Category.query.with_enitities(Category.categoryId, Category.categoryName).distinct().all()
+    return render_template('batches.html', batches=batches[::-1], listAll=False, courses=courses, categories=categories)
 
 @views.route('/categories', methods=['GET', 'POST'])
 def categories():
