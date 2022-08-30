2ed38a3745e6ac925e7f71f5f74ee16ab54f5c16
Prince Johnson <prince.j@inapp.com>
2022-08-30 10:56:55 +0530

    changed

--- website/__init__.py
+++ website/__init__.py
@@ -27,6 +27,5 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+    db.create_all(app=app)
+    print('Created Database!')
